c2f0c636fa0649146512bbcff089535aec11e58d
fiatjaf <fiatjaf@alhur.es>
2021-02-07 21:35:00 -0300

    now I can produce a signature, but is it correct?

--- example/example.dart
+++ example/example.dart
@@ -1,6 +1,17 @@
+import 'dart:core';
+import 'dart:math';
 import 'package:bip340/bip340.dart' as bip340;
 
-
 void main() {
-  print('hello world');
+  print("hello world, let's sign some messages.");
+
+  BigInt key = BigInt.parse(
+      "b493d48364afe44d11c0165cf470a4164d1e2609911ef998be868d46ade3de4e",
+      radix: 16);
+  String message = 'dart is a programming language like the others';
+  String aux =
+      "6216cddf83a0a3b7b1948541806adff3b4d457be49885db225877685576a4014";
+  var signature = bip340.sign(key, message, aux);
+  print(
+      "the signature for '$message' with the key '${key.toRadixString(16)}' and auxiliary data '$aux' is '$signature'");
 }
--- lib/src/bip340.dart
+++ lib/src/bip340.dart
@@ -1,5 +1,6 @@
 import 'dart:core';
 import 'dart:math';
+import 'dart:convert';
 import 'package:convert/convert.dart';
 import 'package:elliptic/elliptic.dart';
 import './helpers.dart';
@@ -7,15 +8,15 @@ import './helpers.dart';
 var secp256k1 = EllipticCurve.fromHexes(
   "secp256k1",
   'fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f',
-  '0000000000000000000000000000000000000000000000000000000000000007',
   'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141',
+  '0000000000000000000000000000000000000000000000000000000000000007',
   '79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798',
   '483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8',
   256,
 );
 
 String sign(BigInt privateKey, String message, String aux) {
-  var bmessage = hex.decode(message);
+  var bmessage = utf8.encode(message);
   var baux = hex.decode(aux);
 
   if ((privateKey < one) || (privateKey > (secp256k1.N - one))) {
