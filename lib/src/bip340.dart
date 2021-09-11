09978952f59453a20af82602b0c8e1bf49121caa
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-09-11 12:05:41 +0200

    Migrate to null safety.
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lib/src/bip340.dart
+++ lib/src/bip340.dart
@@ -1,6 +1,4 @@
 import 'dart:core';
-import 'dart:math';
-import 'dart:convert';
 import 'package:convert/convert.dart';
 import 'package:pointycastle/ecc/api.dart';
 import './helpers.dart';
@@ -21,10 +19,10 @@ String sign(String privateKey, String message, String aux) {
     throw new Error();
   }
 
-  ECPoint P = secp256k1.G * d0;
+  ECPoint P = (secp256k1.G * d0)!;
 
   BigInt d;
-  if (P.y.toBigInteger() % BigInt.two == BigInt.zero) {
+  if (P.y!.toBigInteger()! % BigInt.two == BigInt.zero) {
     // even
     d = d0;
   } else {
@@ -40,7 +38,7 @@ String sign(String privateKey, String message, String aux) {
   BigInt k0 = bigFromBytes(
         taggedHash(
           "BIP0340/nonce",
-          bigToBytes(t) + bigToBytes(P.x.toBigInteger()) + bmessage,
+          bigToBytes(t) + bigToBytes(P.x!.toBigInteger()!) + bmessage,
         ),
       ) %
       secp256k1.n;
@@ -49,17 +47,17 @@ String sign(String privateKey, String message, String aux) {
     throw new Error();
   }
 
-  var R = secp256k1.G * k0;
+  var R = (secp256k1.G * k0)!;
 
   BigInt k;
-  if (R.y.toBigInteger() % BigInt.two == BigInt.zero) {
+  if (R.y!.toBigInteger()! % BigInt.two == BigInt.zero) {
     // is even
     k = k0;
   } else {
     k = secp256k1.n - k0;
   }
 
-  var rX = bigToBytes(R.x.toBigInteger());
+  var rX = bigToBytes(R.x!.toBigInteger()!);
   var e = getE(P, rX, bmessage);
 
   List<int> signature = rX + bigToBytes((k + e * d) % secp256k1.n);
@@ -75,7 +73,7 @@ String sign(String privateKey, String message, String aux) {
 /// signature must be 64-bytes hex-encoded, i.e., 128 characters.
 /// It returns true if the signature is valid, false otherwise.
 /// For more information on BIP-340 see bips.xyz/340.
-bool verify(String publicKey, String message, String signature) {
+bool verify(String? publicKey, String message, String signature) {
   List<int> bmessage = hex.decode(message.padLeft(64, '0'));
 
   List<int> bsig = hex.decode(signature.padLeft(128, '0'));
@@ -86,7 +84,7 @@ bool verify(String publicKey, String message, String signature) {
   }
 
   // turn public key into a point (we only get y, but we find out the y)
-  BigInt x = bigFromBytes(hex.decode(publicKey.padLeft(64, '0')));
+  BigInt x = bigFromBytes(hex.decode(publicKey!.padLeft(64, '0')));
   BigInt y;
   try {
     y = liftX(x);
@@ -97,24 +95,24 @@ bool verify(String publicKey, String message, String signature) {
 
   // not sure what these things mean
   BigInt e = getE(P, r, bmessage);
-  ECPoint sG = secp256k1.G * bigFromBytes(s);
-  ECPoint eP_ = P * e;
-  BigInt ePy = curveP - eP_.y.toBigInteger();
-  ECPoint eP = secp256k1.curve.createPoint(eP_.x.toBigInteger(), ePy);
+  ECPoint sG = (secp256k1.G * bigFromBytes(s))!;
+  ECPoint eP_ = (P * e)!;
+  BigInt ePy = curveP - eP_.y!.toBigInteger()!;
+  ECPoint eP = secp256k1.curve.createPoint(eP_.x!.toBigInteger()!, ePy);
 
   // R is something important
-  ECPoint R = sG + eP;
+  ECPoint R = (sG + eP)!;
   if (R.isInfinity) {
     return false;
   }
 
   // now that we have R we get its coords
-  var Rx = R.x.toBigInteger();
-  var Ry = R.y.toBigInteger();
+  var Rx = R.x!.toBigInteger()!;
+  var Ry = R.y!.toBigInteger();
 
   // and we them in these checks which I don't understand
-  if ((Rx.sign == 0 && Ry.sign == 0) ||
-      (Ry % BigInt.two != BigInt.zero /* is odd */) ||
+  if ((Rx.sign == 0 && Ry!.sign == 0) ||
+      (Ry! % BigInt.two != BigInt.zero /* is odd */) ||
       (Rx != bigFromBytes(r))) {
     return false;
   }
@@ -129,6 +127,6 @@ bool verify(String publicKey, String message, String signature) {
 /// Returns a public key as also 32-bytes hex-encoded.
 String getPublicKey(String privateKey) {
   var d0 = BigInt.parse(privateKey, radix: 16);
-  ECPoint P = secp256k1.G * d0;
-  return P.x.toBigInteger().toRadixString(16);
+  ECPoint P = (secp256k1.G * d0)!;
+  return P.x!.toBigInteger()!.toRadixString(16);
 }
